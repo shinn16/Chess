244096f01b92e7a574b00f519bf9698ee436c406
Jason <means41@marshall.edu>
2016-11-11 11:14:25 -0500

    Commit

--- src/Pieces/Bishop.java
+++ src/Pieces/Bishop.java
@@ -1,6 +1,9 @@
 package Pieces;
 
+import Logic.Board;
 import javafx.scene.image.Image;
+import java.math.*;
+import java.util.Arrays;
 
 /**
  * Bishop
@@ -16,6 +19,81 @@ public class Bishop extends MasterPiece{
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public Coordinate[] getMoves(Board board){
+        Coordinate[] finalMoves;
+        Coordinate[] attacks = new Coordinate[0];
+        Coordinate[] moves = new Coordinate[0];
+
+        try{ //check up and left
+            for(int i=1;i<=super.getCoords().getY();i++){
+                if (board.getPiece(super.getCoords().getY() - i, super.getCoords().getX() - i) == null) {
+                    moves = Arrays.copyOf(moves, moves.length + 1); // expand the moves array
+                    moves[moves.length - 1] = new Coordinate(super.getCoords().getX() - i, super.getCoords().getY() - i); // added the new coordinate
+                }else {
+                    if (board.getPiece(super.getCoords().getY() - i, super.getCoords().getX() - i).getPlayerID() != getPlayerID()){
+                        attacks = Arrays.copyOf(attacks, attacks.length + 1);
+                        attacks[attacks.length - 1] = new Coordinate(super.getCoords().getX() - i, super.getCoords().getY() - i);
+                        break;
+                    } else break;
+                }
+            }
+        }
+        catch(IndexOutOfBoundsException e){}
+
+        try{ //check up and right
+            for(int i=1;i<=super.getCoords().getY();i++){
+                if (board.getPiece(super.getCoords().getY() - i, super.getCoords().getX() + i) == null) {
+                    moves = Arrays.copyOf(moves, moves.length + 1); // expand the moves array
+                    moves[moves.length - 1] = new Coordinate(super.getCoords().getX() + i, super.getCoords().getY() - i); // added the new coordinate
+                }else {
+                    if (board.getPiece(super.getCoords().getY() - i, super.getCoords().getX() + i).getPlayerID() != getPlayerID()){
+                        attacks = Arrays.copyOf(attacks, attacks.length + 1);
+                        attacks[attacks.length - 1] = new Coordinate(super.getCoords().getX() + i, super.getCoords().getY() - i);
+                        break;
+                    } else break;
+                }
+            }
+        }
+        catch(IndexOutOfBoundsException e){}
+
+        try{ //check down and right
+            for(int i=1;i<=5;i++){
+                if (board.getPiece(super.getCoords().getY() + i, super.getCoords().getX() + i) == null) {
+                    moves = Arrays.copyOf(moves, moves.length + 1); // expand the moves array
+                    moves[moves.length - 1] = new Coordinate(super.getCoords().getX() + i, super.getCoords().getY() + i); // added the new coordinate
+                }else {
+                    if (board.getPiece(super.getCoords().getY() + i, super.getCoords().getX() + i).getPlayerID() != getPlayerID()){
+                        attacks = Arrays.copyOf(attacks, attacks.length + 1);
+                        attacks[attacks.length - 1] = new Coordinate(super.getCoords().getX() + i, super.getCoords().getY() + i);
+                        break;
+                    } else break;
+                }
+            }
+        }
+        catch(IndexOutOfBoundsException e){}
+
+        try{ //check down and left
+            for(int i=1;i<=5;i++){
+                if (board.getPiece(super.getCoords().getY() + i, super.getCoords().getX() - i) == null) {
+                    moves = Arrays.copyOf(moves, moves.length + 1); // expand the moves array
+                    moves[moves.length - 1] = new Coordinate(super.getCoords().getX() - i, super.getCoords().getY() + i); // added the new coordinate
+                }else {
+                    if (board.getPiece(super.getCoords().getY() + i, super.getCoords().getX() - i).getPlayerID() != getPlayerID()){
+                        attacks = Arrays.copyOf(attacks, attacks.length + 1);
+                        attacks[attacks.length - 1] = new Coordinate(super.getCoords().getX() - i, super.getCoords().getY() + i);
+                        break;
+                    } else break;
+                }
+            }
+        }
+        catch(IndexOutOfBoundsException e){}
+
+        if (attacks.length == 0) finalMoves = moves;
+        else finalMoves = attacks;
+        return finalMoves;
     }
 }
 
